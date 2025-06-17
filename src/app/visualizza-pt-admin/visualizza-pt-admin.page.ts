8667300f332c373a59be74c2fcbc50ace2134158
Robs109 <roberta.barone03@community.unipa.it>
2025-06-17 18:02:18 +0200

    Visualizza-pt-admin-2

--- src/app/visualizza-pt-admin/visualizza-pt-admin.page.ts
+++ src/app/visualizza-pt-admin/visualizza-pt-admin.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-visualizza-pt-admin',
   templateUrl: './visualizza-pt-admin.page.html',
   styleUrls: ['./visualizza-pt-admin.page.scss'],
+  standalone: false
 })
 export class VisualizzaPtAdminPage implements OnInit {
+  personalTrainers: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit() {
+  this.http.get<any>('http://localhost:5000/api/admin/trainers',)
+    .subscribe({
+      next: (data) => {
+        this.personalTrainers = data.data;
+      },
+      error: (err) => {
+        console.error('Errore nel caricamento dei PT', err);
+      }
+    });
   }
-
 }
+
